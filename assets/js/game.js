2f4337689576c7e10631a5c15f8e489d05411045
Jordan Roberts <jdrobs19@gmail.com>
2020-08-12 14:33:58 -0600

    debugged fight/skip

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,29 @@
+var fightOrSkip = function(){
+    var promptFight = window.prompt ('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+    if (promptFight === "" || promptFight === null){
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    promptFight = promptFight.toLowerCase();
+    
+    if (promptFight === "skip"){
+        var confirmSkip = window.confirm ("Are you sure you'd like to quit?");
+
+        if (confirmSkip){
+            window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+            playerInfo.money = playerInfo.money - 10;
+            return true;
+        }
+    }
+};
+
 var fight = function (enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
-        // ask user if they'd liked to fight or run
-        var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-
-        // if user picks "skip" confirm and then stop the loop
-        if (promptFight === "skip" || promptFight === "SKIP") {
-            // confirm user wants to skip
-            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-
-            // if yes (true), leave fight
-            if (confirmSkip) {
-                window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-                // subtract money from playerInfo.money for skipping
-                playerInfo.money = Math.max(0, playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money)
-                break;
-            }
+        
+        if (fightOrSkip()){
+            break;
         }
 
         // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
